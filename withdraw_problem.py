d28cdb33d1c814bd0029fef1e9d61b6705fc9b2a
rafaelongithub <rafaelnogueiraemail@gmail.com>
2021-06-13 10:31:56 -0300

    Create withdraw_problem.py

---
+++ withdraw_problem.py
@@ -0,0 +1,23 @@
+# This is problem from code chefe web site
+# https://www.codechef.com/problems/HS08TEST
+
+account_amount = 2000.00
+
+# check if withdraw amount valid
+def is_valid(withdraw_amount):
+    if (withdraw_amount % 5) == 0:
+        return True
+    else:
+        return False
+
+finishe = 2
+
+while finishe != 1:
+    withdraw_amount = float(input("Type the amount that you want withdraw:"))
+    if(is_valid(withdraw_amount)):
+        account_amount = account_amount - (withdraw_amount + 0.50)
+    else:
+     print("You have not typed a valid value to withdraw.")
+
+    print(account_amount)
+    finishe = int(input("Type 1 to finishe the program.\nOr type other thing to cotinue."))
